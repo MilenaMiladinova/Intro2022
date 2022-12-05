61515ada24789b06898d64c97cb5b34045d57670
MilenaMiladinova <106926831+MilenaMiladinova@users.noreply.github.com>
2022-12-05 19:17:06 +0200

    Domashno1 completed.

--- src/main/java/programming/intro/Domashno1.java
+++ src/main/java/programming/intro/Domashno1.java
@@ -7,11 +7,12 @@ public class Domashno1 {
         System.out.println("Please enter your username:");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
-        System.out.println("Hello " + username + ".Entry accepted.");
+        System.out.println("Hello " + username + ". Entry accepted.");
         System.out.println("Please provide your hobby:");
         String hobby = scanner.nextLine();
-        System.out.println(username + "," + " how cool is that!");
 
-        // ctrl+alt+L - beautify code
+        System.out.println(username + "," + " how cool is that!");
+        // ctrl+alt+enter - beautify code
     }
 }
+
